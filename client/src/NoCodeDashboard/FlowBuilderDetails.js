769b0044eba08a166a55162e672e83afaafdb80a
mahsook3 <mahsook.m.a@gmail.com>
2025-03-11 20:02:23 +0530

    remove unused components and files from NoCodeDashboard and Builder

--- client/src/NoCodeDashboard/FlowBuilderDetails.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FlowBuilderDetails() {
-  return (
-    <div>FlowBuilderDetails</div>
-  )
-}
--- client/src/NoCodeDashboard/Questionnaires.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Questionnaires() {
-  return (
-    <div>Questionnaires</div>
-  )
-}
